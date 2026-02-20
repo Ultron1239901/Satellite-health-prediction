5e62c7f5414e99ad27e5c2788b3d400435e59f80
Ultron1239901 <vaibhavdhrudev@gmail.com>
2026-02-21 03:38:35 +0530

    frontend

--- frontend/src/components/TelemetryForm.jsx
+++ frontend/src/components/TelemetryForm.jsx
@@ -14,7 +14,7 @@ export default function TelemetryForm() {
   const [result, setResult] = useState(null)
 
   const submit = async () => {
-    const res = await fetch("http://127.0.0.1:8000/predict", {
+    const res = await fetch("https://satellite-health-prediction-q7a8.onrender.com/predict", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
